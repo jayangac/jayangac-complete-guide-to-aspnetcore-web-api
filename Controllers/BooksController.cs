a46014754f7b68ea9923abb0505820c89cfdc384
WINDOWS-PRI5HCL\Jayanga <jayanga.chandrakumara@qualitapps.com>
2021-07-02 07:36:04 +0530

    2. Building Your First  Asp.Net Core API

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBooks.Data.ViewModels;
+using MyBooks.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBooks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private BooksService _booksService;
+        public BooksController(BooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+        [HttpPost("SaveBook")]
+        public IActionResult AddBook([FromBody] BookVM book)
+        {
+            _booksService.AddBook(book);
+            return Ok();
+        }
+
+        [HttpGet("GetAllBooks")]
+        public IActionResult GetAllBooks()
+        {
+            var allBooks = _booksService.GetAllBooks();
+            return Ok(allBooks);
+        }
+
+        [HttpGet("GetBookById/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _booksService.GetBookById(id);
+            return Ok(book);
+        }
+
+        [HttpPut("UpdateBookById/{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody] BookVM book)
+        {
+            var updatedBook = _booksService.UpdateBookById(id, book);
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("DeleteBookbyId/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+            _booksService.DeleteBookById(id);
+            return Ok();
+        }
+    }
+}
---
+++ Services/BooksService.cs
@@ -0,0 +1,75 @@
+﻿using MyBooks.Data;
+using MyBooks.Data.Models;
+using MyBooks.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBooks.Services
+{
+    public class BooksService
+    {
+        private AppDbContext _context;
+        public BooksService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBook(BookVM book)
+        {
+            var bookObj = new Book()
+            {
+                Title = book.Title,
+                Descriptions = book.Descriptions,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead.Value : null,
+                Rate = book.Rate,
+                Genre = book.Genre,
+                Author = book.Author,
+                CoverUrl = book.CoverUrl,
+                DateAdded = DateTime.Now
+            };
+            _context.Books.Add(bookObj);
+            _context.SaveChanges();
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return _context.Books.ToList();
+        }
+
+        public List<Book> GetAllBook() => _context.Books.ToList();
+
+        public Book GetBookById(int bookId) => _context.Books.FirstOrDefault(x => x.Id == bookId);
+
+        public Book UpdateBookById(int bookId, BookVM book)
+        {
+            var _book = _context.Books.FirstOrDefault(x => x.Id == bookId);
+            if (_book != null)
+            {
+                _book.Title = book.Title;
+                _book.Descriptions = book.Descriptions;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.IsRead ? book.DateRead.Value : null;
+                _book.Rate = book.Rate;
+                _book.Genre = book.Genre;
+                _book.Author = book.Author;
+                _book.CoverUrl = book.CoverUrl;
+                _context.SaveChanges();
+            }
+
+            return _book;
+        }
+
+        public void DeleteBookById(int bookId)
+        {
+            var _book = _context.Books.FirstOrDefault(x => x.Id == bookId);
+            if(_book != null)
+            {
+                _context.Books.Remove(_book);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
